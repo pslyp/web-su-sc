a15746a9b04f57e6ff4b9f4941a08098f976940e
PSlyp <nonny_038@hotmail.com>
2018-10-06 23:19:14 +0700

    add sample index page

---
+++ index.php
@@ -0,0 +1,518 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Faculty Of Science - Silpakorn University</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Unica University Template">
+	<meta name="keywords" content="event, unica, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/font-thaisans.css"/>	<!-- Font Thai Sans -->
+	<link rel="stylesheet" href="css/themify-icons.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+	<!--[if lt IE 9]>xxx
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Header section -->
+	<?php 
+		include 'header.php'; 
+	?>
+	<!-- Header section end -->
+
+
+	<!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item set-bg" data-setbg="img/lightroom/SCSU_1-2.jpg" >
+				<div class="hs-text">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8">
+								<div class="hs-subtitle" style="color:#F8941D;">ยินดีต้อนรับ</div>
+								<h2 class="hs-title">คณะวิทยาศาสตร์ <br>มหาวิทยาลัยศิลปากร</h2>
+								<p class="hs-des">คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร <br>วิทยาเขตพระราชวังสนามจันทร์ อำเภอเมือง 73000</p>
+								<div class="site-btn" style="background-color:#F8941D">เข้าสู่ระบบ</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hs-item set-bg" data-setbg="img/lightroom/Collegian_1-2.jpg">
+				<div class="hs-text">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8">
+								<div class="hs-subtitle" style="color:#F8941D"">Welcome</div>
+								<h2 class="hs-title">Faculty of Science<br>Silpakorn University</h2>								
+								<p class="hs-des">Faculty of Science Silpakorn University Sanam Chan Palace Amphoe Mueang 73000</p>
+								<div class="site-btn" style="background-color:#F8941D">Login</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+
+	<!-- Counter section  -->
+	<section class="counter-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 col-md-6">
+					<div class="big-icon">
+						<i class="fa fa-graduation-cap"></i>
+					</div>
+					<div class="counter-content">
+						<h2>นับถอยหลัง : เปิดภาคการศึกษาใหม่</h2>
+						<p><i class="fa fa-calendar-o"></i>14 สิงหาคม 2561</p>
+					</div>
+				</div>
+				<div class="col-lg-5 col-md-6">
+					<div class="counter">
+						<div class="counter-item"><h4>20</h4>Days</div>
+						<div class="counter-item"><h4>08</h4>Hrs</div>
+						<div class="counter-item"><h4>40</h4>Mins</div>
+						<div class="counter-item"><h4>56</h4>secs</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Counter section end -->
+
+
+
+	<!-- Services section -->
+	<section class="service-section spad">
+		<div class="container services">
+
+			<div class="row">
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon" style="background-color:white;">
+						<img src="img/news-icon/1.png" alt="1">
+					</div>
+					<div class="service-content">
+						<a href="sc_news.php"><h4>ข่าวประชาสัมพันธ์</h4></a><hr>
+						<a href="single-blog.php" style="color:gray;">- โครงการปรับพื้นฐาน สำหรับนักศึกษา...</a><br>
+						<a href="single-blog.php" style="color:gray;">- ตารางเวรรักษาการนอกเวลาในการ...</a><br>
+						<a href="single-blog.php" style="color:gray;">- รับสมัครนักศึกษา ประจำปี 2561...</a><br>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/news-icon/2.png" alt="1">
+					</div>
+					<div class="service-content">
+						<a href="sc_news.php"><h4>ข่าวบริการการศึกษา</h4></a><hr>
+						<a href="single-blog.php" style="color:gray;">- รับสมัครนักศึกษา ประจำปี 2561...</a><br>
+						<a href="single-blog.php" style="color:gray;">- การรับพระราชทานปริญญาบัตร...</a><br>
+						<a href="single-blog.php" style="color:gray;">- ตารางเวรรักษาการนอกเวลาในการ...</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/news-icon/3.png" alt="1">
+					</div>
+					<div class="service-content">
+						<a href="sc_news.php"><h4>ข่าวทุนการศึกษา</h4></a><hr>
+						<a href="single-blog.php" style="color:gray;">- รับสมัครนักศึกษา ประจำปี 2561...</a><br>
+						<a href="single-blog.php" style="color:gray;">- การรับพระราชทานปริญญาบัตร...</a><br>
+						<a href="single-blog.php" style="color:gray;">- ตารางเวรรักษาการนอกเวลาในการ...</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/news-icon/4.png" alt="1">
+					</div>
+					<div class="service-content">
+						<a href="sc_news.php"><h4>ข่าวรับสมัครงาน</h4></a><hr>
+						<a href="single-blog.php" style="color:gray;">- รับสมัครนักศึกษา ประจำปี 2561...</a><br>
+						<a href="single-blog.php" style="color:gray;">- การรับพระราชทานปริญญาบัตร...</a><br>
+						<a href="single-blog.php" style="color:gray;">- ตารางเวรรักษาการนอกเวลาในการ...</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/news-icon/5.png" alt="1">
+					</div>
+					<div class="service-content">
+						<a href="sc_news.php"><h4>ข่าวอบรม</h4></a><hr>
+						<a href="single-blog.php" style="color:gray;">- รับสมัครนักศึกษา ประจำปี 2561...</a><br>
+						<a href="single-blog.php" style="color:gray;">- การรับพระราชทานปริญญาบัตร...</a><br>
+						<a href="single-blog.php" style="color:gray;">- ตารางเวรรักษาการนอกเวลาในการ...</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 service-item">
+					<div class="service-icon">
+						<img src="img/news-icon/6.png" alt="1">
+					</div>
+					<div class="service-content">
+						<a href="sc_news.php"><h4>ข่าวทุนวิจัย</h4></a><hr>
+						<a href="single-blog.php" style="color:gray;">- รับสมัครนักศึกษา ประจำปี 2561...</a><br>
+						<a href="single-blog.php" style="color:gray;">- การรับพระราชทานปริญญาบัตร...</a><br>
+						<a href="single-blog.php" style="color:gray;">- ตารางเวรรักษาการนอกเวลาในการ...</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Services section end -->
+
+	
+	<!-- Enroll section -->
+	<section class="enroll-section spad set-bg" data-setbg="img/lightroom/silpakornDoor-1.png">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5">
+					<div class="section-title text-white">
+						<h3>การบริจาค</h3>
+						<p>บริจาคสบทบทุนให้กับทาวคณะวิทยาศาสตร์</p>
+					</div>
+					<div class="enroll-list text-white">
+						<div class="enroll-list-item">
+							<span style="background-color:#F8941D">1</span>
+							<h5>Contact</h5>
+							<p>Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue.</p>
+						</div>
+						<div class="enroll-list-item">
+							<span style="background-color:#F8941D">2</span>
+							<h5>Consulting</h5>
+							<p>Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue.</p>
+						</div>
+						<div class="enroll-list-item">
+							<span style="background-color:#F8941D">3</span>
+							<h5>Register</h5>
+							<p>Lorem ipsum dolor sitdo amet, consectetur dont adipis elit. Vivamus interdum ultrices augue.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-6 offset-lg-1 p-lg-0 p-4">
+					<img src="img/lightroom/graduated_5-2.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Enroll section end -->
+
+
+	<!-- Courses section -->
+	<section class="courses-section spad" style="background-color:rgb(250, 249, 250;">
+		<div class="container">
+			<div class="section-title text-center">
+				<h3>หลักสูตรที่เปิดสอน</h3>
+				<p>12 ปริญาตรี 10 ปริญาโท 8 ปริญาเอก</p>
+			</div>
+			<div class="row">
+				<!-- course item -->
+				<div class="col-lg-4 col-md-6 course-item">
+					<div class="course-thumb" style="background-color:pink;">
+						<img src="img/course/bachelor_icon.png" alt="" style="width:"50%" height:"";">
+						<div class="course-cat">
+							<span style="background-color:#F8941D">ปริญญาตรี</span>
+						</div>
+					</div>
+					<div class="course-info">
+						<div class="date"><i class="fa fa-clock-o"></i> 22 Mar 2018</div>
+						<h4>Certificate Course in Writing<br>for a Global Market</h4>
+						<h4 class="cource-price">฿xx,xxx<span>/เทอม</span></h4>
+					</div>
+				</div>
+				<!-- course item -->
+				<div class="col-lg-4 col-md-6 course-item">
+					<div class="course-thumb">
+						<img src="img/course/master_icon.png" width="30%" height="" alt="">
+						<div class="course-cat">
+							<span style="background-color:#F8941D">ปริญญาโท</span>
+						</div>
+					</div>
+					<div class="course-info">
+						<div class="date"><i class="fa fa-clock-o"></i> 22 Mar 2018</div>
+						<h4>Google AdWords: Get More<br> Customers with Search Marketing </h4>
+						<h4 class="cource-price">฿xx,xxx<span>/เทอม</span></h4>
+					</div>
+				</div>
+				<!-- course item -->
+				<div class="col-lg-4 col-md-6 course-item">
+					<div class="course-thumb">
+						<img src="img/course/PhD_icon.png" width="30%" height="" alt="">
+						<div class="course-cat">
+							<span style="background-color:#F8941D">ปริญญาเอก</span>
+						</div>
+					</div>
+					<div class="course-info">
+						<div class="date"><i class="fa fa-clock-o"></i> 22 Mar 2018</div>
+						<h4>The Ultimate Drawing Course<br> Beginner to Advanced</h4>
+						<h4 class="cource-price">฿xx,xxx<span>/เทอม</span></h4>
+					</div>
+				</div>
+				
+
+			</div>
+		</div>
+	</section>
+	<!-- Courses section end-->
+
+
+	<!-- Fact section -->
+	<section class="fact-section spad set-bg" data-setbg="img/fact-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-lg-3 fact">
+					<div class="fact-icon">
+						<i class="ti-crown"></i>
+					</div>
+					<div class="fact-text">
+						<h2 style="color:#F8941D">47</h2>
+						<p>YEARS</p>
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-lg-2 fact">
+					<div class="fact-icon">
+						<i class="ti-briefcase"></i>
+					</div>
+					<div class="fact-text">
+						<h2 style="color:#F8941D">157</h2>
+						<p>TEACHERS</p>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-2 fact">
+					<div class="fact-icon">
+						<i class="ti-crown"></i>
+					</div>
+					<div class="fact-text">
+						<h2 style="color:#F8941D">47</h2>
+						<p>STAFF</p>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-2 fact">
+					<div class="fact-icon">
+						<i class="ti-user"></i>
+					</div>
+					<div class="fact-text">
+						<h2 style="color:#F8941D">2700</h2>
+						<p>STUDENTS</p>
+					</div>
+				</div>
+				<div class="col-sm-12 col-lg-3 col-xs-12 fact">
+					<div class="fact-icon">
+						<i class="ti-pencil-alt"></i>
+					</div>
+					<div class="fact-text">
+						<h2 style="color:#F8941D">800+</h2>
+						<p>LESSONS</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Fact section end-->
+
+
+	<!-- Event section -->
+	<section class="event-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<h3>กิจกรรม</h3>
+				<p>Our department  initiated a series of events</p>
+			</div>
+			<div class="row">
+				<div class="col-md-6 event-item">
+					<div class="event-thumb">
+						<img src="img/lightroom/SC43_1.jpg" alt="">
+						<div class="event-date">
+							<span style="background-color:#F8941D">4 June 2018</span>
+						</div>
+					</div>
+					<div class="event-info">
+						<h4>GoodBye Senior SC43</h4>
+						<p><i class="fa fa-calendar-o"></i> 06:00 PM - 10:30 PM <i class="fa fa-map-marker"></i> คณะวิทยาศาสตร์</p>
+						<a href="" class="event-readmore">REGISTER <i class="fa fa-angle-double-right"></i></a>
+					</div>
+				</div>
+				<div class="col-md-6 event-item">
+					<div class="event-thumb">
+						<img src="img/lightroom/kkk-2.jpg" alt="">
+						<div class="event-date">
+							<span style="background-color:#F8941D">22 Mar 2018</span>
+						</div>
+					</div>
+					<div class="event-info">
+						<h4>University interview tips:<br>confidence won't make up for flannel</h4>
+						<p><i class="fa fa-calendar-o"></i> 08:00 AM - 10:00 AM <i class="fa fa-map-marker"></i> Center Building, Block A</p>
+						<a href="" class="event-readmore">REGISTER <i class="fa fa-angle-double-right"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Event section end -->
+
+
+	<!-- Gallery section -->
+	<div class="gallery-section">
+		<div class="gallery">
+			<div class="grid-sizer"></div>
+			<div class="gallery-item gi-big set-bg" data-setbg="img/lightroom/gallery/4.jpg">
+				<a class="img-popup" href="img/lightroom/gallery/4.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="img/lightroom/gallery/2-1.png">
+				<a class="img-popup" href="img/lightroom/gallery/2.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="img/lightroom/gallery/5-1.png">
+				<a class="img-popup" href="img/lightroom/gallery/5.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item gi-long set-bg" data-setbg="img/lightroom/gallery/1-1.png">
+				<a class="img-popup" href="img/lightroom/gallery/1.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item gi-big set-bg" data-setbg="img/lightroom/gallery/8.jpg">
+				<a class="img-popup" href="img/lightroom/gallery/8.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item gi-long set-bg" data-setbg="img/lightroom/gallery/3-1.png">
+				<a class="img-popup" href="img/lightroom/gallery/3.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="img/lightroom/gallery/7-1.png">
+				<a class="img-popup" href="img/lightroom/gallery/7.jpg"><i class="ti-plus"></i></a>
+			</div>
+			<div class="gallery-item set-bg" data-setbg="img/lightroom/gallery/9-1.png">
+				<a class="img-popup" href="img/lightroom/gallery/9.jpg"><i class="ti-plus"></i></a>
+			</div>
+		</div>
+	</div>
+	<!-- Gallery section -->
+
+
+	<!-- Blog section -->
+	<section class="blog-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<h3>SCSU Youtube Chanel</h3>
+				<!-- <p>Get latest breaking news & top stories today</p> -->
+			</div>
+			<div class="row">
+				<div class="col-xl-6">
+					<div class="blog-item">
+						<a href="https://www.youtube.com/watch?v=CEmhBEUJKlw" target="_blank"><div class="blog-thumb set-bg" data-setbg="img/SCSU_Youtube/1.png"></div></a>
+						<div class="blog-content">
+							<a href="https://www.youtube.com/watch?v=CEmhBEUJKlw" target="_blank"><h4>ภาควิชาฟิสิกส์ คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร</h4></a>
+							<div class="blog-meta">
+								<span><i class="fa fa-calendar-o"></i> 25 Mar 2018</span>
+								<span><i class="fa fa-user"></i> Physics Science Silpakorn</span>
+							</div>
+							<p>แนะนำภาควิชาฟิสิกส์ คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6">
+					<div class="blog-item">
+						<a href="https://www.youtube.com/watch?v=afxEndYtQSg" target="_blank"><div class="blog-thumb set-bg" data-setbg="img/SCSU_Youtube/2.png"></div></a>
+						<div class="blog-content">
+							<a href="https://www.youtube.com/watch?v=afxEndYtQSg" target="_blank"><h4>ภาควิชาฟิสิกส์ คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร</h4></a>
+							<div class="blog-meta">
+								<span><i class="fa fa-calendar-o"></i> 25 Mar 2018</span>
+								<span><i class="fa fa-user"></i>Physics Science Silpakorn</span>
+							</div>
+							<p>แนะนำภาควิชาฟิสิกส์ คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6">
+					<div class="blog-item">
+						<a href="https://www.youtube.com/watch?v=Ow9GaxzG9us" target="_blank"><div class="blog-thumb set-bg" data-setbg="img/SCSU_Youtube/3.png"></div></a>
+						<div class="blog-content">
+							<a href="https://www.youtube.com/watch?v=Ow9GaxzG9us" target="_blank"><h4>สาธิตการทำปฏิบัติการฟิสิกส์ เรื่อง แอมมิเตอร์ และโวลต์มิเตอร์</h4></a>
+							<div class="blog-meta">
+								<span><i class="fa fa-calendar-o"></i> 13 Mar 2018</span>
+								<span><i class="fa fa-user"></i> Physics Science Silpakorn
+							</span>
+							</div>
+							<p>สาธิตการทำปฏิบัติการฟิสิกส์ เรื่อง แอมมิเตอร์ และโวลต์มิเตอร์
+								ภาควิชาฟิสิกส์ คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร
+								สามารถติดตามวีดีโออื่นๆได้ที่ www.phy.sc.su.ac.th</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6">
+					<div class="blog-item">
+						<a href="https://www.youtube.com/watch?v=0drQDowLYGs" target="_blank"><div class="blog-thumb set-bg" data-setbg="img/SCSU_Youtube/4.png"></div></a>
+						<div class="blog-content">
+							<a href="https://www.youtube.com/watch?v=0drQDowLYGs" target="_blank"><h4>สาธิตการทำปฏิบัติการ เรื่อง โวลต์ โอห์ม มิลลิแอมมิเตอร์</h4></a>
+							<div class="blog-meta">
+								<span><i class="fa fa-calendar-o"></i> 13 Mar 2018</span>
+								<span><i class="fa fa-user"></i> Physics Science Silpakorn
+							</span>
+							</div>
+							<p>สาธิตการทำปฏิบัติการ เรื่อง โวลต์ โอห์ม มิลลิแอมมิเตอร์
+								ภาควิชาฟิสิกส์ คณะวิทยาศาสตร์ มหาวิทยาลัยศิลปากร
+								สามารถติดตามวีดีโออื่นๆได้ที่ www.phy.sc.su.ac.th</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Blog section -->
+
+
+	
+	<!-- Newsletter section -->
+	<section class="newsletter-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5 col-lg-7">
+					<div class="section-title mb-md-0">
+					<h3>NEWSLETTER</h3>
+					<p>Subscribe and get the latest news and useful tips, advice and best offer.</p>
+				</div>
+				</div>
+				<div class="col-md-7 col-lg-5">
+					<form class="newsletter">
+						<input type="text" placeholder="Enter your email">
+						<button class="site-btn">SUBSCRIBE</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Newsletter section end -->
+	
+	
+	
+	<!-- Footer section -->
+	<?php
+		include 'footer.php';
+	?>
+	<!-- Footer section end-->
+
+	
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.countdown.js"></script>
+	<script src="js/masonry.pkgd.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+</body>
+</html>
